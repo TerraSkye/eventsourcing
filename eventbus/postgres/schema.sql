6804d18e0484dfe01136fff652ad435d94789032
Roelof Jan <roelof.jan@hey.com>
2026-06-11 15:20:11 +0200

    Use Listen & Notify for postgres eventbus

--- eventbus/postgres/schema.sql
+++ eventbus/postgres/schema.sql
@@ -2,3 +2,16 @@ CREATE TABLE IF NOT EXISTS event_subscriptions (
     name     VARCHAR PRIMARY KEY,
     position BIGINT  NOT NULL DEFAULT 0
 );
+
+CREATE OR REPLACE FUNCTION eventsourcing_notify_events_inserted() RETURNS trigger AS $$
+BEGIN
+    PERFORM pg_notify('eventsourcing_events_inserted', '');
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS eventsourcing_events_notify ON events;
+CREATE TRIGGER eventsourcing_events_notify
+    AFTER INSERT ON events
+    FOR EACH STATEMENT
+    EXECUTE FUNCTION eventsourcing_notify_events_inserted();
--- go.mod
+++ go.mod
@@ -64,9 +64,9 @@ require (
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.49.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
-	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
