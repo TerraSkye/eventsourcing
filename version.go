17e99ffe7a8acfa66d09d3e0b8f5d74279c3aa35
S Riemersma <5426161+TerraSkye@users.noreply.github.com>
2026-07-30 13:59:52 +0200

    Bump InstrumentationVersion to v0.1.7

--- version.go
+++ version.go
@@ -3,5 +3,5 @@ package eventsourcing
 // InstrumentationVersion is the module version reported as the
 // instrumentation version of this package's OpenTelemetry meter and tracer.
 const (
-	InstrumentationVersion = "v0.1.0"
+	InstrumentationVersion = "v0.1.7"
 )
